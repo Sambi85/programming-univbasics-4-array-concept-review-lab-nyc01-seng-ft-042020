dc1e8dc7bc1ac10024fed5b23f381a9398bf388e
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-16 02:03:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
 i = 0
 
 while i < array.length do
-    array[i] =   value_to_find
+    array[i] = value_to_find
    puts array.index(value_to_find) 
     
     i += 1
